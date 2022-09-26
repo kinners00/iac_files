36bcd163086fadd10ce75e64cefc8f6fa4b9fbf3
Marc McKinley <marcmckinley@me.com>
2022-09-26 17:18:21 +0100

    Update aws_example.tf

--- files/terraform/aws_example.tf
+++ files/terraform/aws_example.tf
@@ -34,4 +34,23 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
     status = "Enabled"
     mfa_delete = "Enabled"
   }
+  
+ resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.b.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.7f39f2ee-61c2-489a-89bf-586a890ee2b9.arn:aws:kms:us-east-1:487527143985:key/7f39f2ee-61c2-489a-89bf-586a890ee2b9
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
